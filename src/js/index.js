bff9e0ce8e4878b9ddd8a86410283b4215bd69d6
Rajiv Toora <rajiv.toora@tranzact.net>
2024-01-29 21:05:45 -0500

    add subtraction function and test cases

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,8 @@ function sum(a, b) {
     return a + b;
 }
 
-module.exports = sum;
+function subtraction(a, b) {
+    return a - b;
+}
+
+module.exports = { sum, subtraction };
--- src/js/index.test.js
+++ src/js/index.test.js
@@ -1,9 +1,17 @@
-const sum = require('./index');
+const operations = require('./index');
 
 test('add 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(operations.sum(1, 2)).toBe(3);
 });
 
 test('add 2 + 3 to equal 5', () => {
-    expect(sum(2, 3)).toBe(5);
+    expect(operations.sum(2, 3)).toBe(5);
+});
+
+test('subtract 1 - 1 equal 0', () => {
+    expect(operations.subtraction(1, 1)).toBe(0);
+});
+
+test('subtract 2 - 1 equal 1', () => {
+    expect(operations.subtraction(2, 1)).toBe(1);
 });
